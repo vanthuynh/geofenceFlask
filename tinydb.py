5c89c329f49691459224e7b9a7fb1b64abd66fe2
Van Huynh <vthuynh@cpp.edu>
2022-01-21 22:10:22 -0800

    keep only necessary object and pause here

--- tinydb.py
+++ tinydb.py
@@ -29,55 +29,6 @@ geoMAC = [
                 }
             }
         ]
-geoERU = [
-            {
-                "Coordinates": [
-                    {"lat": 0.0, "lng": 0.0}
-                ],
-                "Keep_in": True,
-                "Circle_inputs": {
-                    "lng": 0.0,
-                    "lat": 0.0,
-                    "rad": None
-                }
-            },
-            {
-                "Coordinates": [
-                    {"lat": 0.0, "lng": 0.0}
-                ],
-                "Keep_in": False,
-                "Circle_inputs": {
-                    "lng": 0.0,
-                    "lat": 0.0,
-                    "rad": None
-                }
-            }
-        ]
-geoMEA = [
-            {
-                "Coordinates": [
-                    {"lat": 0.0, "lng": 0.0}
-                ],
-                "Keep_in": True,
-                "Circle_inputs": {
-                    "lng": 0.0,
-                    "lat": 0.0,
-                    "rad": None
-                }
-            },
-            {
-                "Coordinates": [
-                    {"lat": 0.0, "lng": 0.0}
-                ],
-                "Keep_in": False,
-                "Circle_inputs": {
-                    "lng": 0.0,
-                    "lat": 0.0,
-                    "rad": None
-                }
-            }
-        ]
-
 # @app.route('/',methods = ['GET'])
 # def ping():
 #     return jsonify(geoMEA)
@@ -86,35 +37,20 @@ geoMEA = [
 SUBMIT ALL: clear all data and add new submitted data
 DELETE ALL: clear all data and leave it be
 '''
-@app.route('/MAC', methods=['GET', 'POST'])
-def add_geo_MAC():
+@app.route('/geofence/<vehicle_id>', methods=['GET', 'POST'])
+def submit_geofence():
     response_object = {'status': 'success'}
     if request.method == 'POST':
         geoData = request.get_json()
-        geoMAC.clear()
-        #add keep in data
-        geoMAC.append({
-            'Coordinates': geoData[0].get('Coordinates'),
-            'Keep_in': geoData[0].get('Keep_in'),
-            'read': geoData[0].get('read')
-        })
-        #add keep out data
-        geoMAC.append({
-            'Coordinates': geoData[1].get('Coordinates'),
-            'Keep_in': geoData[1].get('Keep_in'),
-            'read': geoData[1].get('read')
-        })
+        
         response_object['message'] = 'data added!'
     else:
         response_object['data'] = geoMAC
     return jsonify(response_object)
 
-@app.route('/MAC', methods=['DELETE'])
-def remove_geo_MAC():
-    response_object = {'status': 'success'}
-    geoMAC.clear()
-    response_object['message'] = 'data removed!'
-    return jsonify(response_object)
+@app.route('/geofence/<vehicle_id>', methods=['DELETE'])
+def remove_geofence(vehicle_id):
+    
 
 
 if __name__ == '__main__':
